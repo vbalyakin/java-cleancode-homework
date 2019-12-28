915e052206b6419a0ee862de3abbbbbe844dc5b3
Valiantsin Baliakin <valentinakaryuk@gmail.com>
2019-12-28 23:52:24 +0300

    code cleaning task

---
+++ src/main/java/Planes/ExperimentalPlane.java
@@ -0,0 +1,26 @@
+package Planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalTypes;
+
+public class ExperimentalPlane extends Plane {
+    private ExperimentalTypes experimentalTypes;
+    private ClassificationLevel classificationLevel;
+
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes experimentalTypes, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.experimentalTypes = experimentalTypes;
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ClassificationLevel getClassificationLevel() {
+        return classificationLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "experimentalPlane{" +
+                "model='" + model + '\'' +
+                '}';
+    }
+}
